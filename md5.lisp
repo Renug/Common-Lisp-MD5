1cd01b8080225f8cb904c5df552389230d769343
Renug <liang.rannger@gmail.com>
2012-11-18 21:56:20 +0800

    first commit

---
+++ md5.lisp
@@ -0,0 +1,88 @@
+(defpackage :liang.rannger.md5
+  (:use :common-lisp)
+  (:export :md5))
+
+(defvar *r-list* '(7 12 17 22 7 12 17 22 7 12 17 22  7 12 17 22 5  9 14 20  5  9 14 20  5  9 14 20  5  9 14 20 4 11 16 23  4 11 16 23  4 11 16 23  4 11 16 23 6 10 15 21  6 10 15 21  6 10 15 21  6 10 15 21))
+
+(defvar *k-list* (loop for i from 0 to 63 collect 
+		      (floor (* (expt 2 32) (abs (sin (+ i 1)))))))
+
+(defun string-to-code-list (string)
+  (loop for x across string collect (char-code x)))
+
+(defmacro while (test &body body)
+  `(do ()
+       ((not ,test))
+     ,@body))
+
+(defmacro nappend (lst1 lst2)
+  (let ((lst1-sym (gensym))
+	(lst2-sym (gensym)))
+    `(let ((,lst2-sym ,lst2) (,lst1-sym ,lst1))
+       (setf ,lst1 (append ,lst1-sym ,lst2-sym)))))
+
+(defun left-rotate (num rotate-bit &optional (length-in-bit 32))
+  (if (and (numberp num) (numberp rotate-bit))
+      (let ((right-part (ldb (byte rotate-bit (- length-in-bit rotate-bit)) num))
+	    (left-part (ldb (byte (- length-in-bit rotate-bit) 0) num)))
+	(+ right-part (* (expt 2 rotate-bit) left-part)))))
+
+(defun pre-processing (lst)
+  (let ((ret-list (copy-list lst)))
+    (nappend ret-list '(#x80))
+    (while (/= (mod (length ret-list) 64) 56) 
+      (nappend ret-list '(0)))
+    (let ((len (* (length lst) 1)))
+	  (loop for x from 0 to 7 do 
+	       (nappend ret-list (list (ldb (byte 8 (* x 8)) len)))))
+    ret-list))
+
+(defun md5 (string)
+  (labels ((combo-list (lst &optional (sublist-length 4))
+	     (if (or (null lst) (< (length lst) sublist-length))
+		 lst
+		 (append (list (subseq lst 0 sublist-length)) (combo-list (subseq lst  sublist-length)))))
+	   (byte-to-dword (lst)
+	     (let ((result 0))
+	       (dolist (item lst result)
+		 (incf result (+ (* (expt 2 (* 8 (position item lst))) item)))))))
+  (let ((512bit-list (combo-list (pre-processing (string-to-code-list string)) 64))
+	(a #x67452301)
+	(b #xEFCDAB89)
+	(c #x98BADCFE)
+	(d #x10325476)
+	(f 0)
+	(g 0)
+	(h0 #x67452301)
+	(h1 #xEFCDAB89)
+	(h2 #x98BADCFE)
+	(h3 #x10325476))
+    (loop for lst in 512bit-list 
+	 do(let ((dword-list (mapcar #'reverse (apply #'combo-list 512bit-list))))
+	     (format t "~x" (byte-to-dword '(#xab #xcd #x12 #x34)))
+		  (dotimes (i 63)
+		    (cond 
+		      ((and (<= 0 i) (<= i 15))
+		       (setf f (logior (logand b c) (logand (lognot b) d)))
+		       (setf g i))
+		      ((and (<= 16 i) (<= i 31))
+		       (setf f (logior (logand d b) (logand (lognot d) c)))
+		       (setf g (mod (1+ (* 5 i)) 16)))
+		      ((and (<= 32 i) (<= i 47))
+		       (setf f (logxor b c d))
+		       (setf g (mod (+ 5 (* 3 i)) 16)))
+		      ((and (<= 48 i) (<= i 63))
+		       (setf f (logxor c (logior b (lognot d))))
+		       (setf g (mod (* 7 i) 16))))
+		    (let ((temp d))
+		      (setf d c)
+		      (setf c b)
+		      (incf b (left-rotate (+ a f (elt *k-list* i) (byte-to-dword (elt dword-list g))) (elt *r-list* i)))
+		      (setf a temp)))
+		  (incf h0 a)
+		  (incf h1 b)
+		  (incf h2 c)
+		  (incf h3 d)))
+    (format nil "~8x ~8x ~8x ~8x" h0 h1 h2 h3))))
+	       
+      
