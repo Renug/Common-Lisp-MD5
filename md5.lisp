16e13b042326e4ad270d1d91f1c9b432f9fa0657
Renug <liang.rannger@gmail.com>
2012-11-24 23:04:04 +0800

    better?

--- md5.lisp
+++ md5.lisp
@@ -19,11 +19,6 @@
 (defun string-to-code-list (string)
   (loop for x across string collect (char-code x)))
 
-(defmacro while (test &body body)
-  `(do ()
-       ((not ,test))
-     ,@body))
-
 (defun left-rotate (num rotate-bit &optional (length-in-bit 32))
   (if (and (numberp num) (numberp rotate-bit))
       (logior (ldb (byte length-in-bit 0) (ash num rotate-bit)) (ash num (- rotate-bit length-in-bit)))))
@@ -37,8 +32,8 @@
 (defun pre-processing (lst)
   (let ((ret-list (copy-list lst)))
     (nappend ret-list '(#x80))
-    (while (/= (mod (length ret-list) 64) 56)
-      (nappend ret-list '(0)))
+    (loop while (/= (mod (length ret-list) 64) 56) 
+       do (nappend ret-list '(0)))
     (append ret-list (loop for i from 0 to 7 collect (ldb (byte 8 (* i 8)) (* 8 (length lst)))))))
 
 (defun combo-list (lst &optional (sublist-length 4))
@@ -46,8 +41,8 @@
 		 lst
 		 (append (list (subseq lst 0 sublist-length)) (combo-list (subseq lst sublist-length) sublist-length))))
 
-(defmacro 32integer (num)
-  `(ldb (byte 32 0) ,num))
+(defmacro 32integer+ (&rest args)
+  `(ldb (byte 32 0) (+ ,@args)))
 
 (defun dword-to-byte (num)
   (list (ldb (byte 8 0) num) 
@@ -73,28 +68,28 @@
 		  (dotimes (i 64)
 		    (cond 
 		      ((and (<= 0 i) (<= i 15))
-		       (setf f (32integer (logior (logand b c) (logandc1 b d))))
+		       (setf f (logior (logand b c) (logandc1 b d)))
 		       (setf g i))
 		      ((and (<= 16 i) (<= i 31))
-		       (setf f (32integer (logior (logand d b) (logandc1 d c))))
+		       (setf f (logior (logand d b) (logandc1 d c)))
 		       (setf g (mod (1+ (* 5 i)) 16)))
 		      ((and (<= 32 i) (<= i 47))
-		       (setf f (32integer (logxor b c d)))
+		       (setf f (logxor b c d))
 		       (setf g (mod (+ 5 (* 3 i)) 16)))
 		      ((and (<= 48 i) (<= i 63))
-		       (setf f (32integer (logxor c (logorc2 b d))))
+		       (setf f (logxor c (logorc2 b d)))
 		       (setf g (mod (* 7 i) 16))))
 		      (let ((temp d))
 			(setf d c)
 			(setf c b)
-			(setf b (32integer (+ b  (left-rotate 
+			(setf b (32integer+ b  (left-rotate 
 						  (32integer (+ a f (elt *k-list* i) (byte-to-dword (elt dword-list g)))) 
-						  (elt *r-list* i)))))
+						  (elt *r-list* i))))
 			(setf a temp)))
-		  (setf h0 (32integer (+ h0 a)))
-		  (setf h1 (32integer (+ h1 b)))
-		  (setf h2 (32integer (+ h2 c)))
-		  (setf h3 (32integer (+ h3 d)))))
+		  (setf h0 (32integer+ h0 a))
+		  (setf h1 (32integer+ h1 b))
+		  (setf h2 (32integer+ h2 c))
+		  (setf h3 (32integer+ h3 d))))
     (append (dword-to-byte h0) (dword-to-byte h1) (dword-to-byte h2) (dword-to-byte h3))))
 
 
