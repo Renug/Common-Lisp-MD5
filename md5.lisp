7963cf7851dcbd30ac9e0dad8d5e565aa6980cd8
Renug <liang.rannger@gmail.com>
2012-11-24 16:08:31 +0800

    v

--- md5.lisp
+++ md5.lisp
@@ -7,16 +7,8 @@
 			 4  11  16  23   4  11  16  23   4  11  16  23   4  11  16  23
 			 6  10  15  21   6  10  15  21   6  10  15  21   6  10  15  21))
 
-;(defvar *k-list* (loop for i from 0 to 63 collect 
-;		      (floor (* (expt 2 32) (abs (sin (+ i 1)))))))
-
-(defparameter *k-list* '(#xd76aa478 #xe8c7b756 #x242070db #xc1bdceee #xf57c0faf #x4787c62a #xa8304613 #xfd469501 #x698098d8
-			 #x8b44f7af #xffff5bb1 #x895cd7be #x6b901122 #xfd987193 #xa679438e #x49b40821 #xf61e2562 #xc040b340
-			 #x265e5a51 #xe9b6c7aa #xd62f105d #x2441453  #xd8a1e681 #xe7d3fbc8 #x21e1cde6 #xc33707d6 #xf4d50d87 
-			 #x455a14ed #xa9e3e905 #xfcefa3f8 #x676f02d9 #x8d2a4c8a #xfffa3942 #x8771f681 #x6d9d6122 #xfde5380c
-			 #xa4beea44 #x4bdecfa9 #xf6bb4b60 #xbebfbc70 #x289b7ec6 #xeaa127fa #xd4ef3085 #x4881d05 #xd9d4d039
-			 #xe6db99e5 #x1fa27cf8 #xc4ac5665 #xf4292244 #x432aff97 #xab9423a7 #xfc93a039 #x655b59c3 #x8f0ccc92
-			 #xffeff47d #x85845dd1 #x6fa87e4f #xfe2ce6e0 #xa3014314 #x4e0811a1 #xf7537e82 #xbd3af235 #x2ad7d2bb #xeb86d391))
+(defvar *k-list* (loop for i from 0 to 63 collect 
+		      (floor (* (expt 2 32) (abs (sin (float i 0.0d0)))))))
 
 (defun byte-to-dword (lst)
   (destructuring-bind (byte1 byte2 byte3 byte4) lst
@@ -75,16 +67,16 @@
 		  (dotimes (i 63)
 		    (cond 
 		      ((and (<= 0 i) (<= i 15))
-		       (setf f (32integer (logior (logand b c) (logand (lognot b) d))))
+		       (setf f (32integer (logior (logand b c) (logandc1 b d))))
 		       (setf g i))
 		      ((and (<= 16 i) (<= i 31))
-		       (setf f (32integer (logior (logand d b) (logand (lognot d) c))))
+		       (setf f (32integer (logior (logand d b) (logandc1 d c))))
 		       (setf g (mod (1+ (* 5 i)) 16)))
 		      ((and (<= 32 i) (<= i 47))
 		       (setf f (32integer (logxor b c d)))
 		       (setf g (mod (+ 5 (* 3 i)) 16)))
 		      ((and (<= 48 i) (<= i 63))
-		       (setf f (32integer (logxor c (logior b (lognot d)))))
+		       (setf f (32integer (logior c (logorc2 b d))))
 		       (setf g (mod (* 7 i) 16))))
 		    (let ((temp (32integer d)))
 		      (setf d (32integer c))
